05c3a20d8a9e1c1ee4024979063475c2c7c536be
Dhanashree Patil <dhanashreec.gem@gmail.com>
2026-03-20 17:55:27 +0530

    [BFF-3] WIP for ground holding qc API's

--- src/main/java/com/gemengserv/bff/controller/GroundHoldingSafetyController.java
+++ src/main/java/com/gemengserv/bff/controller/GroundHoldingSafetyController.java
@@ -17,7 +17,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/indospacesafety")
+@RequestMapping("/groundholdingsafety")
 public class GroundHoldingSafetyController
 {
     @Autowired
