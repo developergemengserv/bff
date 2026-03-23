b96fbe1b23efad1c52a829abdda41736e7073e7f
Dhanashree Patil <dhanashreec.gem@gmail.com>
2026-03-23 16:37:57 +0530

    [BFF-5] Adding all API's of hcc safety project in bff

---
+++ src/main/java/com/gemengserv/bff/controller/GemSafetyController.java
@@ -0,0 +1,12 @@
+package com.gemengserv.bff.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/gemsafety")
+public class GemSafetyController
+{
+
+}
