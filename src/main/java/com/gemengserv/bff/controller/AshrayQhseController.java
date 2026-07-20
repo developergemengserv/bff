d8daa2a5efe2490408705f19ae98a7a833aa4390
Dhanashree Patil <dhanashreec.gem@gmail.com>
2026-07-20 18:26:46 +0530

    Hazards & equipments add, update, fetch master api added in ashray, gem, ashwinsheth

--- src/main/java/com/gemengserv/bff/controller/AshrayQhseController.java
+++ src/main/java/com/gemengserv/bff/controller/AshrayQhseController.java
@@ -1310,4 +1310,39 @@ public class AshrayQhseController
         return ashrayQhseService.addDeviceTokenAndAppVersion(userId, token, deviceToken, appVersion);
     }
 
+    @RequestMapping(value = "/addHazard", method = RequestMethod.POST)
+    public Map<String, Object> addHazard(@RequestParam(value = "label") String label) {
+        return ashrayQhseService.addHazard(label);
+    }
+
+    @RequestMapping(value = "/addEquipment", method = RequestMethod.POST)
+    public Map<String, Object> addEQ(@RequestParam(value = "label") String label) {
+        return ashrayQhseService.addEQ(label);
+    }
+
+    @RequestMapping(value = "/updateHazard", method = RequestMethod.POST)
+    @ResponseBody // Make sure this is present if you aren't using @RestController
+    public Map<String, Object> updateHazard(
+            @RequestParam(value = "label") String label,
+            @RequestParam(value = "id") int id) {
+        return ashrayQhseService.updateHazard(label, id);
+    }
+
+    @RequestMapping(value = "/updateEquipment", method = RequestMethod.POST)
+    @ResponseBody // Make sure this is present if you aren't using @RestController
+    public Map<String, Object> updateEQ(
+            @RequestParam(value = "label") String label,
+            @RequestParam(value = "id") int id) {
+        return ashrayQhseService.updateEQ(label, id);
+    }
+
+    @RequestMapping(value = "/getHazards", method = RequestMethod.GET)
+    public ResponseEntity<Map<String, Object>> getHazards() {
+        return ashrayQhseService.getHazards();
+    }
+
+    @RequestMapping(value = "/getEquipments", method = RequestMethod.GET)
+    public ResponseEntity<Map<String, Object>> getEquipments() {
+        return ashrayQhseService.getEquipments();
+    }
 }
